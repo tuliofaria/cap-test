c1c7406bb62465d008bdefdf902f9340f88e2efe
Tulio Faria <tuliofaria@gmail.com>
2015-10-22 22:07:29 -0200

    initial.

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(port, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
