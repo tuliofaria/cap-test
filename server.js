039a0e158c35e690eb75f360c6bee3c2d2c0f3fa
Tulio Faria <tuliofaria@gmail.com>
2015-10-23 09:41:21 -0200

    okay.

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ var server = app.listen(port, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Testing BizeApp at http://%s:%s', host, port);
 });
